2de664ee993d111f137e42323d791f91741da844
AlexandrStopkin <stnalex1@gmail.com>
2023-03-16 15:57:19 +0100

    Add constructor with one argument in IncorrectWeightException

--- src/exception/IncorrectWeightException.java
+++ src/exception/IncorrectWeightException.java
@@ -1,4 +1,7 @@
 package exception;
 
 public class IncorrectWeightException extends IllegalArgumentException {
+  public IncorrectWeightException(int weight) {
+    super("Вес не может быть отрицательным: " + weight);
+  }
 }
