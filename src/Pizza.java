17afcc972d8ebc041afbcde6432b8b9440d013c9
AlexandrStopkin <stnalex1@gmail.com>
2023-03-16 16:07:08 +0100

    Add Task2 add field maxWeight add setter setMaxWeight

--- src/Pizza.java
+++ src/Pizza.java
@@ -12,12 +12,37 @@ public class Pizza {
 //
 //  Дополните файл Pizza.java из классной работы, выбросив в конструкторе исключение
 //  IncorrectWeightException при попытке создать пиццу с отрицательной массой.
+
+//  Задача 2* (не обязательно)
+//  Дополните файлы Pizza.java и PizzaRunner.java из классной работы (и задачи № 1).
+//
+//  Добавьте классу Pizza приватный статический атрибут - максимально возможный вес пиццы.
+//
+//  Добавьте классу Pizza публичный статический метод (сеттер) для изменения этого атрибута.
+//
+//  При запуске программы PizzaRunner.java должна быть возможность:
+//
+//  запустить программу как раньше, без аргументов, тогда ограничения нет
+//  (Или есть? подсказка: Integer.MAX_VALUE).
+//  запустить программу с одним аргументом командной строки: целым числом.
+//  Тогда именно это целое число и будет максимально возможным весом пиццы (в граммах).
+//  Примечание
+//  Вы также можете проверить параметр командно строки на корректность и использовать
+//  IncorrectWeightException из задачи 1 в случае некорректного запуска программы.
+
   private String title;
   private int weight;
+  private static int maxWeight;
+
+  public static void setMaxWeight(int weigh){
+    maxWeight = weigh;
+  }
 
   public Pizza(String title, int weight) {
     this.title = title;
-    if (weight < 0) throw new IncorrectWeightException(weight);
+    if (weight < 0) {
+      throw new IncorrectWeightException(weight);
+    }
     this.weight = weight;
   }
 
